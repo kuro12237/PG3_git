67a95812c22aa1fad2fb0866ef74ba5810e39833
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-10-02 12:21:44 +0900

    save

--- main.cpp
+++ main.cpp
@@ -1,10 +1,73 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 
-
-int main()
+template<typename T>
+T Min(T a,T b)
 {
+	if (a<b){
+		return a;
+	}
+	if (b<a){
+		return b;
+	}
+};
 
-	printf("Hello");
+template<>
+int Min<int>(int a, int b)
+{
+	if (a < b) {
+		printf("%d\n",a);
+		return a;
+	}
+	if (b < a) {
+		printf("%d\n", b);
+		return b;
+	}
+};
+
+template<>
+float Min<float>(float a, float b)
+{
+	if (a < b) {
+		printf("%f\n", a);
+		return a;
+	}
+	if (b < a) {
+		printf("%f\n", b);
+		return b;
+	}
+};
+
+template<>
+double Min<double>(double a, double b)
+{
+	if (a < b) {
+		printf("%lf\n", a);
+		return a;
+	}
+	if (b < a) {
+		printf("%lf\n", b);
+		return b;
+	}
+};
+
+template<>
+char Min<char>( char a, char b)
+{
+	printf("数字以外は代入できません\n");
+	return 0;
+}
+
+
+int main(void)
+{
+	//int
+	Min<int>(512, 6);
+	//float
+    Min<float>(40.5f, 60.0f);
+	//double
+    Min<double>(30.5, 70.0);
+	//char
+	Min<char>('a', 'g');
 
 	return 0;
 }
