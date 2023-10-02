5dfd8f8dbeefecc8ff6459c8caded432579e8e3e
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-10-02 12:41:16 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -18,22 +18,18 @@ char Min<char>( char a, char b)
 	return 0;
 }
 
-
-
 int main(void)
 {
 
 	//int
 	printf("%d\n",Min<int>(512, 6));
 	
-
 	//float
 	printf("%f\n", Min<float>(40.5f, 60.0f));
 
 	//double
     printf("%lf\n",Min<double>(30.5, 70.0));
 
-	
 	//char
 	Min<char>('a', 'g');
 
