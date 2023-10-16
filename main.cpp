3c3c1e0db520b34c4f270eda9f0f7295e9423c9c
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-10-16 13:00:34 +0900

    01_03_

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<random>
+#include<Windows.h>
+
+using namespace std;
+typedef void (*DiceGame)(int *);
+
+int AnsDice(void) 
+{
+	random_device seedGenerator;
+	uniform_int_distribution<int>distribution(1,6);
+	int result = distribution(seedGenerator);
+	return result;
+}
+string IsIven(int num)
+{
+	if (num % 2 == 0)
+	{
+		return "‹ô”";
+	}
+	else
+	{
+		return "Šï”";
+	}
+}
+
+void SelectNumber(int *number)
+{
+	int ans = AnsDice();
+
+	printf("‘I‘ğ‚µ‚½”‚Í%d‚Å‚·\n", *number);
+	printf("Dice‚Í%d‚Ì–Ú‚ªo‚Ü‚µ‚½\n", ans);
+	printf("‘I‘ğ‚µ‚½”‚Í%c‚Å‚µ‚½\n", IsIven(*number));
+	printf("Dice‚Í%c‚Å‚µ‚½\n", IsIven(ans));
+
+	if (*number % 2 == 0 == ans % 2 == 0)
+	{
+		printf("YouWin\n");
+	}
+	else
+	{
+		printf("YouLose\n");
+	}
+}
+
+void TimeCount(DiceGame d,int SelectNumber,int &Timer,bool &IsSelect)
+{
+	Sleep(Timer * 1000);
+	d(&SelectNumber);
+	IsSelect = false;
+}
+
+int main() {
+	int TestScanf;
+	int Timer = 3;
+	bool isSelect = false;
+
+	DiceGame diceGame;
+	diceGame = SelectNumber;
+
+	while (true)
+	{
+		if (!isSelect)
+		{
+			printf("1‚©2key‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+			scanf_s("%d", &TestScanf);
+
+			if (TestScanf<=0||TestScanf>=3)
+			{
+				continue;
+			}
+			isSelect = true;
+		}
+	
+		if (isSelect)
+		{
+			TimeCount(diceGame, TestScanf, Timer,isSelect);
+		}
+	}
+	return 0;
+}
