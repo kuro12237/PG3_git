feecbfeb9894a9ad3027d3317e3b4b5b8be324c2
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2024-01-29 21:11:23 +0900

    06_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,65 +1,27 @@
 #include <stdio.h>
-#include <list>
 #include <iostream>
+#include<vector>
+#include<thread>
 
 using namespace std;
 
+void DrawText(const string text)
+{
+	cout << text << endl;
+}
+
 int main() {
 
-	list<const char*> Station{
-		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata","Komagome",
-		"Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
-		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho","Shimbashi","Yurakucyo"
+	vector<string> texts{ "thread 1","thread 2","thread 3" };
 
-	};
+	thread th1(DrawText, texts[0]);
+	th1.join();
 
-	cout << "山手線全駅" << "\n";
-	cout << "1970年、2019年、2022年の一覧を表示" << "\n";
-	cout << "表示したい年を入力" << "\n";
+	thread th2(DrawText, texts[1]);
+	th2.join();
 
-
-	//入力
-	int selectYear = 0;
-	cin >> selectYear;
-
-	//1970年
-	if (selectYear == 1970) {
-		cout << "1970年の駅一覧を表示" << "\n";
-	}
-
-	//2019年
-	if (selectYear == 2019) {
-		cout << "2019年の駅一覧を表示" << "\n";
-		for (list<const char*>::iterator itr = Station.begin(); itr != Station.end(); ++itr) {
-			if (*itr == "Tabata") {
-				itr = Station.insert(itr, "Nishi-Nippori");
-				++itr;
-			}
-		}
-
-	}
-
-	//2022年
-	if (selectYear == 2022) {
-		cout << "2022年の駅一覧を表示" << "\n";
-		for (list<const char*>::iterator itr = Station.begin(); itr != Station.end(); ++itr) {
-			if (*itr == "Tabata") {
-				itr = Station.insert(itr, "Nishi-Nippori");
-				++itr;
-			}
-			if (*itr == "Tamachi") {
-				itr = Station.insert(itr, "Takanawa GateWay");
-				++itr;
-			}
-		}
-
-	}
-
-	//追加した後に表示
-	for (list<const char*>::iterator itr = Station.begin(); itr != Station.end(); ++itr) {
-
-		cout << *itr << "\n";
-	}
+	thread th3(DrawText, texts[2]);
+	th3.join();
 
 	return 0;
 }
